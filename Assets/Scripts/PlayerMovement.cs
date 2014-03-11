860ce922e145b636a781c2b42e29fbd08bf8c1ff
Andre Schloemp <andre.schloemp@me.com>
2014-03-11 21:07:05 +0100

    z

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,7 +16,6 @@ public class PlayerMovement : MonoBehaviour
   }
 
   protected Vector3 fForce = Vector3.zero;
-  protected Vector3 fLastForce = Vector3.zero;
 
   void FixedUpdate ()
   {
@@ -37,25 +36,6 @@ public class PlayerMovement : MonoBehaviour
         fForce = Vector3.zero;
       }
     }
-    /*
-    float v = rigidbody.velocity.magnitude;
-    if (v > 10.0f) {
-      int anz = Mathf.RoundToInt(v / 2);
-      for (int i = 0; i < anz; i++) {
-        ParticleSystem ps = transform.FindChild ("Speedy").particleSystem;
-        Vector3 d = rigidbody.velocity / 2.0f;
-        Vector3 p = transform.position;
-        float delta = 0.2f;
-        p.y += -1.0f + Random.Range (-delta, delta);
-        d.x += Random.Range (-delta, delta);
-        d.y += Random.Range (-delta, delta);
-        d.z += Random.Range (-delta, delta);
-        ps.Emit (transform.position, d, 0.2f, 5.0f, new Color32(128,128,128,128));
-        ParticleSystem.Particle p = new ParticleSystem.Particle();
-        ps.
-      }
-    }
-    */
   }
 
   public Vector3 CalcForce (float aH, float aV)
@@ -93,7 +73,6 @@ public class PlayerMovement : MonoBehaviour
       } else {
         aF = Vector3.zero;
       }
-      //Debug.Log("V" + v.ToString() + " c" + c.ToString() + " aF" + aF.ToString());
     } 
     rigidbody.AddForce (aF);
   }
@@ -103,58 +82,5 @@ public class PlayerMovement : MonoBehaviour
   {
     fForce = aForce;
   }
-
-  /*
-  void OnTriggerEnter(Collider collider) {
-    if (collider.gameObject.tag == "Ground") {
-      Terrain t = collider.gameObject.GetComponent<Terrain>();
-      if (t != null) {
-        Vector3 pos = collider.ClosestPointOnBounds( transform.position );
-        DropDownTerrainAt(t, pos);
-      }
-    }
-  }
-  
-  void OnCollisionEnter (Collision collision)
-  {
-    if (collision.gameObject.tag == "Ground") {
-      Terrain t = collision.gameObject.GetComponent<Terrain>();
-      if (t != null) {
-        ContactPoint contact = collision.contacts [0];
-        Vector3 pos = contact.point;
-        DropDownTerrainAt(t, pos);
-      }
-    }
-  }
-
-  public void DropDownTerrainAt(Terrain lTerrain, Vector3 pos) {
-    int lw = 2;
-    int lh = 2;
-    Vector3 lOffset = pos - lTerrain.transform.position;
-    lOffset.x += ( lw / lTerrain.terrainData.size.x ) / 2;
-    lOffset.z += ( lh / lTerrain.terrainData.size.z ) / 2;
-    int lx = Mathf.FloorToInt( ( ( lOffset.x % lTerrain.terrainData.size.x ) / lTerrain.terrainData.size.x ) * lTerrain.terrainData.heightmapWidth );
-    int ly = Mathf.FloorToInt( ( ( lOffset.z % lTerrain.terrainData.size.z ) / lTerrain.terrainData.size.z ) * lTerrain.terrainData.heightmapHeight );
-    //print (lTerrain.terrainData.size.ToString() + "/" + lOffset.ToString() + ":" + lx.ToString() + "," + ly.ToString());
-    float[,] lHeights = lTerrain.terrainData.GetHeights(lx,ly,lw,lh);
-    for(int lxi = 0; lxi < lw; lxi++) {
-      for(int lyi = 0; lyi < lh; lyi++) {
-        lHeights[lxi,lyi] -= 0.00005f;
-      }
-    }
-    lTerrain.terrainData.SetHeights(lx,ly, lHeights);
-    float[,,] lMaps = lTerrain.terrainData.GetAlphamaps(lx,ly, lw, lh);
-    for(int lxi = 0; lxi < lw; lxi++) {
-      for(int lyi = 0; lyi < lh; lyi++) {
-        //lMaps[lxi,lyi,4] = 1.0f;
-        lMaps[lxi,lyi,3] = 1.0f;
-        lMaps[lxi,lyi,2] = 0.0f;
-        lMaps[lxi,lyi,1] = 0.0f;
-        lMaps[lxi,lyi,0] = 0.0f;
-      }
-    }
-    lTerrain.terrainData.SetAlphamaps(lx, ly, lMaps);
-  }
-  */
 
 }
